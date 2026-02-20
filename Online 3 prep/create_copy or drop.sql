74a69eccc0ab2dbba79ca800bcde02fe3f7fe4de
Mithun-11 <sanimmuntasirmithun@gmail.com>
2026-02-20 23:30:34 +0600

    a

--- Online 3 prep/create_copy or drop.sql
+++ Online 3 prep/create_copy or drop.sql
@@ -3,3 +3,9 @@ DROP TABLE your_table_name;
 CREATE TABLE your_new_table_name AS 
 SELECT * FROM your_original_table_name;
 COMMIT;
+
+CREATE TABLE your_table_name (
+    column1_name DATA_TYPE,
+    column2_name DATA_TYPE,
+    column3_name DATA_TYPE
+);
